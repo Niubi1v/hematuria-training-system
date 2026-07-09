1183686baf42a80db83fbf714fc369dbfbec5b96
Dr.Lambo <niubilv@gmail.com>
2026-07-09 12:12:07 +0800

    Fix GitHub Pages case route links

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  trailingSlash: true,
+  trailingSlash: false,
   basePath,
   assetPrefix: basePath ? `${basePath}/` : undefined,
   images: {
